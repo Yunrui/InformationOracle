af40e96099c5691cc8b2710ccc4e73d0bbe7b38c
YunRui SiMa <mylpis@hotmail.com>
2014-09-28 21:20:03 +0800

    delete column family

--- rec/src/RefreshContentData.java
+++ rec/src/RefreshContentData.java
@@ -2,6 +2,8 @@ import java.io.IOException;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.hbase.HBaseConfiguration;
+import org.apache.hadoop.hbase.HColumnDescriptor;
+import org.apache.hadoop.hbase.HTableDescriptor;
 import org.apache.hadoop.hbase.client.*;
 import org.apache.hadoop.hbase.filter.Filter;
 import org.apache.hadoop.hbase.util.Bytes;
@@ -13,15 +15,21 @@ public class RefreshContentData {
         conf.set("hbase.zookeeper.quorum", "hbase");
         conf.set("hbase.zookeeper.property.clientPort", "2181");
 
+        /*
         HTable table = new HTable(conf, "test3");
-                                                    Scan scan = new Scan();
+        Scan scan = new Scan();
+        ResultScanner scanner = table.getScanner(scan);
 
-                                                            ResultScanner scanner = table.getScanner(scan);
-                                                                    for (Result res : scanner) {
-                                                                                    System.out.println(res.toString() + "----" + Bytes.toString(res.value()));
-                                                                                            }
-                                                                                                    scanner.close();
-                                                                                                            table.close();
-                                                                                                                }
+        for (Result res : scanner) {
+            System.out.println(res.toString() + "----" + Bytes.toString(res.value()));
+        }
+
+        scanner.close();
+        table.close();
+        */
+
+        HBaseAdmin admin = new HBaseAdmin(conf);
+        admin.deleteColumn(Bytes.toBytes("test3"), "d");
+    }
 }
 
