01b205a4069e4e97947d6fa24cebf9498645c38b
Yunrui <root@HANSONtekiMacBook-Pro.local>
2014-09-28 11:06:06 +0800

    structure source code

--- rec/src/SequenceFileOperator.java
+++ rec/src/SequenceFileOperator.java
@@ -24,101 +24,106 @@ import org.apache.mahout.math.Vector;
 import org.apache.mahout.math.SequentialAccessSparseVector;
 
 public class SequenceFileOperator {
-  private Configuration conf = new Configuration();
+    private Configuration conf = new Configuration();
 
-  public static void main(String[] args) throws IOException {
-    SequenceFileOperator docToSeqFileWriter = new SequenceFileOperator();
-    String dictionaryFilePath = args[0];
-    String sequenceFilePath = args[1];
-    Map<String, String> dic = docToSeqFileWriter.readDictionary(dictionaryFilePath);
-
-    docToSeqFileWriter.dumpToHBase(dic, sequenceFilePath); 
-  }
-
-  private Map<String, String> readDictionary(String sequenceFilePath) throws IOException {
-    Option filePath = SequenceFile.Reader.file(new Path(sequenceFilePath));
-    SequenceFile.Reader sequenceFileReader = new SequenceFile.Reader(conf, filePath);
-    Writable key = (Writable) ReflectionUtils.newInstance(sequenceFileReader.getKeyClass(), conf);
-    Writable value = (Writable) ReflectionUtils.newInstance(sequenceFileReader.getValueClass(), conf);
-    
-    Map<String, String> dic = new HashMap<String, String>();
-
-    try {
-      while (sequenceFileReader.next(key, value)) {
-        dic.put(value.toString(), key.toString());
-      }
-    } finally {
-      IOUtils.closeStream(sequenceFileReader);
+    public SequenceFileOperator() {
+        this.conf.setStrings("dfs.namenode.adress", "192.168.59.103:8020");
     }
 
-    return dic;
-  }
+    public static void main(String[] args) throws IOException {
+        SequenceFileOperator docToSeqFileWriter = new SequenceFileOperator();
+        String dictionaryFilePath = args[0];
+        String sequenceFilePath = args[1];
 
-  private void dumpToHBase(Map<String, String> dictionary, String sequenceFilePath) throws IOException {
-    Option filePath = SequenceFile.Reader.file(new Path(sequenceFilePath));
-    SequenceFile.Reader sequenceFileReader = new SequenceFile.Reader(conf, filePath);
-    Writable key = (Writable) ReflectionUtils.newInstance(sequenceFileReader.getKeyClass(), conf);
-    Writable value = (Writable) ReflectionUtils.newInstance(sequenceFileReader.getValueClass(), conf);
-    
+        Map<String, String> dic = docToSeqFileWriter.readDictionary(dictionaryFilePath);
 
-    try {
-      while (sequenceFileReader.next(key, value)) {
-        
-        /*
-        if (!key.toString().contains("highscalability")) {
-          continue;
-        }
-        */
+        docToSeqFileWriter.dumpToHBase(dic, sequenceFilePath);
+    }
 
-        VectorWritable vector = (VectorWritable) value;
+      private Map<String, String> readDictionary(String sequenceFilePath) throws IOException {
+        Option filePath = SequenceFile.Reader.file(new Path(sequenceFilePath));
+        SequenceFile.Reader sequenceFileReader = new SequenceFile.Reader(conf, filePath);
+        Writable key = (Writable) ReflectionUtils.newInstance(sequenceFileReader.getKeyClass(), conf);
+        Writable value = (Writable) ReflectionUtils.newInstance(sequenceFileReader.getValueClass(), conf);
 
-        Map<String, Double> doc = new HashMap<String, Double>();
-        for (Iterator<Vector.Element> iter = ((SequentialAccessSparseVector)vector.get()).iterator(); iter.hasNext(); ) {
-          Vector.Element element = iter.next();
-          
-          String feature = dictionary.get(String.valueOf(element.index()));
-          Double frequency = element.get();
-        
-          if (frequency > 0 && !feature.toString().contains("'")) {
-            doc.put(feature, frequency);
-            
+        Map<String, String> dic = new HashMap<String, String>();
+
+        try {
+          while (sequenceFileReader.next(key, value)) {
+            dic.put(value.toString(), key.toString());
           }
+        } finally {
+          IOUtils.closeStream(sequenceFileReader);
         }
-       
-       int i = 0;
-       List orderedDoc = new LinkedList(sortByValue(doc).entrySet());
-       for(Iterator it = orderedDoc.iterator(); it.hasNext();) {
-         Map.Entry entry = (Map.Entry) it.next();
 
-         if (entry.getValue().toString().contains("\n"))
-           continue;
-
-         System.out.printf("put 'content', '%s', 'd:%s', %f\n", key, entry.getKey(), entry.getValue());
-         
-         if (++i >= 20) {
-           break;
-         }
-       }
-       
+        return dic;
       }
-    } finally {
-      IOUtils.closeStream(sequenceFileReader);
-    }
-  }
 
-  static Map sortByValue(Map map) {
-    List list = new LinkedList(map.entrySet());
-    Collections.sort(list, new Comparator() {
-      public int compare(Object o1, Object o2) {
-        return -((Comparable) ((Map.Entry) (o1)).getValue()).compareTo(((Map.Entry) (o2)).getValue());
+      private void dumpToHBase(Map<String, String> dictionary, String sequenceFilePath) throws IOException {
+        Option filePath = SequenceFile.Reader.file(new Path(sequenceFilePath));
+        SequenceFile.Reader sequenceFileReader = new SequenceFile.Reader(conf, filePath);
+        Writable key = (Writable) ReflectionUtils.newInstance(sequenceFileReader.getKeyClass(), conf);
+        Writable value = (Writable) ReflectionUtils.newInstance(sequenceFileReader.getValueClass(), conf);
+
+
+        try {
+          while (sequenceFileReader.next(key, value)) {
+
+            /*
+            if (!key.toString().contains("highscalability")) {
+              continue;
+            }
+            */
+
+            VectorWritable vector = (VectorWritable) value;
+
+            Map<String, Double> doc = new HashMap<String, Double>();
+            for (Iterator<Vector.Element> iter = ((SequentialAccessSparseVector)vector.get()).iterator(); iter.hasNext(); ) {
+              Vector.Element element = iter.next();
+
+              String feature = dictionary.get(String.valueOf(element.index()));
+              Double frequency = element.get();
+
+              if (frequency > 0 && !feature.toString().contains("'")) {
+                doc.put(feature, frequency);
+
+              }
+            }
+
+           int i = 0;
+           List orderedDoc = new LinkedList(sortByValue(doc).entrySet());
+           for(Iterator it = orderedDoc.iterator(); it.hasNext();) {
+             Map.Entry entry = (Map.Entry) it.next();
+
+             if (entry.getValue().toString().contains("\n"))
+               continue;
+
+             System.out.printf("put 'content', '%s', 'd:%s', %f\n", key, entry.getKey(), entry.getValue());
+
+             if (++i >= 20) {
+               break;
+             }
+           }
+
+          }
+        } finally {
+          IOUtils.closeStream(sequenceFileReader);
+        }
       }
-    });
 
-    Map result = new LinkedHashMap();
-    for (Iterator it = list.iterator(); it.hasNext();) {
-      Map.Entry entry = (Map.Entry)it.next();
-      result.put(entry.getKey(), entry.getValue());
-    }
-    return result;
-  } 
+      static Map sortByValue(Map map) {
+        List list = new LinkedList(map.entrySet());
+        Collections.sort(list, new Comparator() {
+          public int compare(Object o1, Object o2) {
+            return -((Comparable) ((Map.Entry) (o1)).getValue()).compareTo(((Map.Entry) (o2)).getValue());
+          }
+        });
+
+        Map result = new LinkedHashMap();
+        for (Iterator it = list.iterator(); it.hasNext();) {
+          Map.Entry entry = (Map.Entry)it.next();
+          result.put(entry.getKey(), entry.getValue());
+        }
+        return result;
+      }
 }
