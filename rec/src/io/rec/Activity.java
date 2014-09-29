f35f2fdca9cf41eada331978195c163a35410523
YunRui SiMa <mylpis@hotmail.com>
2014-09-29 13:17:02 +0800

    Add Activity

--- rec/src/io/rec/Activity.java
+++ rec/src/io/rec/Activity.java
@@ -17,45 +17,14 @@ import org.apache.hadoop.hbase.client.*;
 import org.apache.hadoop.hbase.filter.Filter;
 import org.apache.hadoop.hbase.util.Bytes;
 
-public class Activity {
+public abstract class Activity {
 
-    public static vod main(String[] args) throws IOException {
-        String zookeeper = args[0];
-        Configuration conf = HBaseConfiguration.create();
-        conf.set("hbase.zookeeper.quorum", zookeeper);
-        conf.set("hbase.zookeeper.property.clientPort", "2181");
-        try {
-            Path pt=new Path("hdfs://localhost:8020/io/meta/dump");
-            FileSystem fs = FileSystem.get(new URI("hdfs://localhost:8020"), new Configuration());
-            BufferedReader br = new BufferedReader(new InputStreamReader(fs.open(pt)));
-            String line;
-            line=br.readLine();
-            while (line != null) {
-                System.out.println(line);
-                line = br.readLine();
-            }
-        }
-        catch(URISyntaxException e) {
-        }
+    public abstract String getName();
 
-        // clear column family d 
-        byte[] name = Bytes.toBytes("content");
-        HBaseAdmin admin = new HBaseAdmin(conf);
-        HTableDescriptor table = admin.getTableDescriptor(name);
-        if (table.hasFamily(Bytes.toBytes("d"))) {
-            table.removeFamily(Bytes.toBytes("d"));
-            admin.disableTable(name);
-            admin.modifyTable(name, table);
-            admin.enableTable(name);
-        }
+    public abstract void DoCore();
 
-        HColumnDescriptor desc = new HColumnDescriptor("d");
-        table.addFamily(desc);
-        admin.disableTable(name);
-        admin.modifyTable(name, table);
-        admin.enableTable(name);
-        admin.close();
-
+    public void Do() {
+        this.DoCore();
     }
 }
 
